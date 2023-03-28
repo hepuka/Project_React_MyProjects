b79249abf9f841c851fc2bf94bc95e3847146730
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-03-28 20:31:48 +0200

    upd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,6 @@ import PageLinks from "./PageLinks";
 const Footer = () => {
   return (
     <footer className="section footer">
-      <PageLinks parentClass="footer-links" itemClass="footer-link" />
-
       <p className="copyright">
         copyright &copy; Zoltan KUN-FAGYAL
         <span id="date">{new Date().getFullYear()}</span>. all rights reserved
